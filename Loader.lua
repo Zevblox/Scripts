efcb1348430084a2aee57ce1d6e760e3d8b0b7c1
Zevblox <zevblox@gmail.com>
2024-12-27 22:52:53 +0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,18 @@
-print("Work in progress...")
+if ZEV_LOADED then
+    return
+end
+
+local cloneref = cloneref or function(o) return o end
+COREGUI = cloneref(game:GetService("CoreGui"))
+
+if not game:IsLoaded() then
+    local notLoaded = Instance.new("Message")
+    notLoaded.Parent = COREGUI
+    notLoaded.Text = "Zevblox is waiting for the game to load"
+    game.Loaded:Wait()
+    notLoaded:Destroy()
+end
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Zevblox/Scripts/refs/heads/main/FindScript.lua'))()
+
+pcall(function() getgenv().ZEV_LOADED = true end)
