ff45a283d9e37fe3b974e625b50e787c0ee4112c
Zevblox <zevblox@gmail.com>
2024-12-30 06:56:11 +0600

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -1,1 +1,65 @@
-print("Work in progress")
+local Plr = game:GetService("Players").LocalPlayer
+local PlotObjects = workspace.Plots:WaitForChild("Plot_" .. "" .. Plr.Name):WaitForChild("House"):WaitForChild("Objects"):GetChildren()
+local WaitTime = 2.6
+
+local RemoteName = game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("DataService"):GetChildren()[1].Name
+local Remote = False
+
+local function FindRemote(Item, args)
+    local newfound = False
+    for _, Folder in pairs(game:GetService("ReplicatedStorage"):WaitForChild("Modules"):WaitForChild("DataService"):GetChildren()) do
+        print("Folder: "..Folder)
+        if Folder:IsA("Folder") and Folder.Name == RemoteName then
+            print("Ok Folder: "..Folder)
+            for _, v in pairs(Folder:GetChildren()) do
+                print("Remote: "..v)
+                if v:IsA("RemoteEvent") and v.Name == RemoteName then
+                    print("Ok Remote: "..v)
+                    v:FireServer(unpack(args))
+                    wait(0.5)
+                    print(#Item:GetChildren())
+                    if Item:FindFirstChild("_attachOccupied") or #Item:GetChildren() == 3 then
+                        Remote = v
+                        newFound = True
+                        print("Remote found...")
+                        break
+                   end
+                end
+            end
+        end
+        wait(0.01)
+    end
+    print("Checked all...")
+end
+
+for _, Obj in pairs(PlotObjects) do
+    if Obj.Name == "Large Ordinary Planter" then
+        local ItemHolder = Obj:FindFirstChild("ItemHolder")
+        if ItemHolder then
+            for _, Item in pairs(ItemHolder:GetChildren()) do
+                if Item.Name == "Berry Bush" and #Item.ObjectData:GetChildren() <= 0 then
+                    Plr.Character.HumanoidRootPart.CFrame = Obj.CFrame + Vector3.new(0, 5.5, 0)
+                    local args = {
+                        [1] = {
+                            ["Target"] = Item,
+                            ["Path"] = "6"
+                        }
+                    }
+                    if Remote then
+                        print("Using saved remote.")
+                        Remote:FireServer(unpack(args))
+                        wait(0.5)
+                        if Item:FindFirstChild("_attachOccupied") == nil then
+                            print("Saved remote didn't work, finding remote...")
+                            FindRemote(Item, args)
+                        end
+                    else
+                        print("No saved remote, finding remote...")
+                        FindRemote(Item, args)
+                    end
+                    wait(WaitTime)
+                end
+           end
+        end
+    end
+end
